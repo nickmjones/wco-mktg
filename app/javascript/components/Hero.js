9c760d846a3ce0e61852c1f7569598e813df8a92
nickmjones <hello@nickmjones.me>
2020-08-12 15:24:39 -0400

    Case studies looking better.

--- app/javascript/components/Hero.js
+++ app/javascript/components/Hero.js
@@ -4,8 +4,8 @@ export default class Hero extends Component {
 
   render() {
     return (
-      <section class="breakout margin-bottom-large">
-        <div class="columns margin-y-xlarge">
+      <section class="breakout margin-y-large">
+        <div class="columns">
           <div class="column column--center-children align-center">
             <p class="large color-quartz">{this.props.superhead}</p>
             <h4 class="display color-admiral">{this.props.headline}</h4>
